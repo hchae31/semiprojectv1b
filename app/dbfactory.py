7e9bfb4a793238d8c7ef0ad893703b09058c69c9
hchae31 <chaeheegon5858@gmail.com>
2024-08-23 09:55:36 +0900

    board 테이블 생성 member의 userid와 외래키 설정

--- app/dbfactory.py
+++ app/dbfactory.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from app.model import member, board
 from app.settings import config
 
 engine = create_engine(config.dbconn, echo=True)
@@ -15,7 +16,8 @@ def get_db():
         db.close()
 
 async def db_startup():
-    pass
+    member.Base.metadata.create_all(engine)
+    board.Base.metadata.create_all(engine)
 
 async def db_shutdown():
     pass
